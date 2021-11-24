9132d9f1824512ac02adc313997f55a6475a9ee0
Nick <speedyleion@users.noreply.github.com>
2021-11-23 17:28:35 -0800

    Initial test and hardcoded return value

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+//          Copyright Nick G 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+use std::time::Duration;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn parse_test_start(_line: &[u8]) -> (String, Duration){
+    ("start_of_a_test".into(), Duration::default())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_single_test_start(){
+        let ctest_output = b" Start 1: start_of_a_test";
+
+        assert_eq!(parse_test_start(ctest_output), ("start_of_a_test".into(), Duration::default()));
+    }
+}
+
